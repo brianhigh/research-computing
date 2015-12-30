640c2103b077916e6051f432bc94afbba69474f4
brianhigh <brianhigh@users.noreply.github.com>
2015-12-29 16:55:12 -0800

    Update get_logo_png.sh

--- get_logo_png.sh
+++ get_logo_png.sh
@@ -2,15 +2,15 @@
 
 # Downloads image and resizes to 128x128px PNG. Requires: wget and convert.
 # You will need ImageMagick: http://www.imagemagick.org/script/index.php
-# Brian High, 2014-05-15, CC BY-SA 4.0, https://github.com/brianhigh
+# Brian High, 2015-12-29, CC BY-SA 4.0, https://github.com/brianhigh
 #
 # Usage: bash get_logo_png.sh 'URL'
 #
 # This script implements something like this example "pipeline":
 #   wget -nv -O - \
 #     'http://www.washington.edu/brand/files/2014/09/W-Logo_Purple_Hex.png' | \
-#     -format png -background transparent -resize 128x128 -gravity center \
-#     -extent 128x128 logo.png logo_128.png
+#     convert -format png -background transparent -resize 128x128 \
+#     -gravity center -extent 128x128 logo.png logo_128.png
     
 # Initialize variables
 test_failed=0
